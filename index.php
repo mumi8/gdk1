ac3d55578f1442e73b4f162ae1d7c748ea0a608d
mumi8 <pasha.biliy@mail.ru>
2015-04-15 11:09:03 +0300

    Add project

---
+++ index.php
@@ -0,0 +1,465 @@
+<!DOCTYPE html>
+<html dir="ltr" lang="ru">
+<head><meta name='yandex-verification' content='6020bf254eff76ba' />
+<meta charset="UTF-8" />
+<title>MagazineS</title>
+
+<base href="localhost" />
+<link rel="stylesheet" type="text/css" href="catalog/view/theme/default/stylesheet.css" />
+<link rel="stylesheet" type="text/css" href="catalog/view/theme/default/slideshow.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="catalog/view/theme/default/timer.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="catalog/view/theme/default/callme_blue.css" media="screen" />
+
+<link rel="stylesheet" type="text/css" href="catalog/view/javascript/jquery/colorbox/colorbox.css" media="screen" />
+<link rel="stylesheet" type="text/css" href="catalog/view/javascript/jquery/ui/themes/jquery-ui-1.8.16.custom.css" />
+
+<link rel="stylesheet" type="text/css" href="catalog/view/theme/default/fastorder.css" />
+
+
+
+
+
+
+</head>
+<body>
+	<head>
+	<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+	<!-- the necessary css for yukari -->	
+	<link rel="stylesheet" type="text/css" media="screen,projection" href="catalog/yukari/yukari.css" />
+	
+	<style type="text/css" media="screen">
+		*{margin:0 auto;}
+		#wrapper {width:90%;margin:15px auto;}
+		p { margin:20px 0;}
+	</style>
+	
+</head>
+</head>
+
+
+<div id="container">
+
+<div id="header">
+
+
+
+
+
+
+ 
+
+
+<div id="logo">
+<a href=""><img src="catalog/view/theme/default/image/logo.png"></a>
+</div>
+
+
+<div id="contact">
+<div id="block_one">
+<img src="catalog/view/theme/default/image/phone.png" alt="" >
+</div>
+<div id="block_two">
+<img src="catalog/view/theme/default/image/contact.png" alt="" >
+</div>
+<div id="block_three">
+<!-- <a href="#" ><img src="catalog/view/theme/default/image/call.png" alt="" ></a> -->
+<a class=" fastorder" data-name="" data-url="http://eco-bell.ru/" href="javascript:void(0)">
+<span></span><img src="catalog/view/theme/default/image/call.png" alt="" ></a>	
+</div>
+</div>
+
+
+<div style="
+position:absolute;
+top:19px;
+right:4px;
+z-index:3000;
+width:301px;
+height:111px;
+">
+
+<a href=" ">
+<div style="width:100%; height:100%"></div>
+</a>
+
+</div>
+
+
+<div id="basket">
+<img src="catalog/view/theme/default/image/basket.png" alt="" >
+ <div id="cart">
+  <div class="heading">
+    <h4>Корзина покупок</h4>
+    
+    <a>
+    
+	
+    <span id="cart-total">В корзине: 0 шт. На сумму: 0р</span></a></div>
+  <div class="content">
+        <div class="empty">Корзина покупок пуста!</div>
+      </div>
+</div></div>
+</a>
+
+
+<div id="reg">
+<a href=" "> Регистрация / Вход</a>
+
+
+
+<input type="text" placeholder="Логин">
+<input type="password" placeholder="Пароль">
+<div id="enter"><a href="#" ><img src="catalog/view/theme/default/image/enter.png" alt="" ></a></div> 
+</div>
+
+<div id="bl1"></div>
+
+
+
+
+
+</div>
+
+
+
+
+       <!-- <div class="anons">
+  <img src="catalog/view/theme/default/image/car2.jpg" alt="">
+  <img src="catalog/view/theme/default/image/car2.jpg" alt="">
+  <img src="catalog/view/theme/default/image/avto.jpg" alt=""> 
+
+
+
+ </div>-->
+<div id="menu2">
+ 
+<div id="prod">
+<a href=" "><img src="catalog/view/theme/default/image/prod.png"></a>
+</div>
+
+ <div id="search">
+    <div class="button-search"></div>
+        <input type="text" name="filter_name" value="Поиск" onclick="this.value = '';" onkeydown="this.style.color = '#000000';" />
+
+      </div>
+   
+      <div class="anons3">
+  <img src="catalog/view/theme/default/image/mm1.png" alt="">
+  <img src="catalog/view/theme/default/image/mmx.png" alt="">
+  <img src="catalog/view/theme/default/image/mm3.png" alt="">
+  <img src="catalog/view/theme/default/image/mm8.png" alt="">
+    <img src="catalog/view/theme/default/image/aa.jpg" alt="">
+      <img src="catalog/view/theme/default/image/aa.jpg" alt="">
+        <img src="catalog/view/theme/default/image/aa.jpg" alt="">
+ </div>
+ 
+</div>
+
+
+<div id="notification"></div>
+<div id="column-left">
+    <div class="box box3 ">
+   <!-- <div class="box-heading">Категории</div> -->
+  <div class="box-content ">
+    <div class="box-category opa2">
+      <ul>
+                <li>
+                    <a href=" ">ПОСТЕЛЬНОЕ БЕЛЬЕ    </a></li>
+                  <li><a href=" ">ПРОСТЫНИ </a></li>  
+                    <li><a href=" ">ПОДОДЕЯЛЬНИКИ </a></li>  
+                      <li><a href=" ">НАВОЛОЧКИ </a></li>
+                        <li><a href=" "> ПОДУШКИ</a></li>  
+                          <li><a href=" "> ОДЕЯЛА</a></li>   
+                            <li><a href=" "> ПОКРЫВАЛА</a></li>   
+                              <li><a href=" "> МАТРАСЫ</a></li>  
+                                <li><a href=" ">ТКАНИ </a></li>  
+                                  <li><a href=" ">ПОЛОТЕНЦА </a></li>  
+                                    <li><a href=" "> ХАЛАТЫ</a></li>  
+                                      <li><a href=" ">СКАТЕРТИ </a></li>  
+                                        <li><a href=" ">ЗАНАВЕСКИ </a></li>  
+                                          <li><a href=" ">КАСТРЮЛИ И КОВИШИ </a></li>  
+                                            <li><a href=" ">СКОВОРОДЫ И СОТЕЙНИКИ </a></li>  
+                                              <li><a href=" ">НАБОРЫ ПОСУДЫ </a></li>  
+                                                <li><a href=" "> КРЫШКИ</a></li>  
+                                                  <li><a href=" "> ФОРМЫ ДЛЯ ВЫПЕСКИ</a></li>  
+                                                    <li><a href=" ">АКСЕССУАРЫ </a></li>  
+                                                      <li><a href=" ">СТОЛОВЫЕ ПРИБОРЫ </a></li>  
+                                                        <li><a href=" ">ЧАЙНИКИ </a></li>  
+                
+              </ul>
+    </div>
+  </div>
+</div>
+    <div class="box"><div class="otz">
+  <div class="box-heading">Сейчас на сайте: </div>
+  <div class="box-content">
+    5 пользователей и 10 гостей.
+    <li>Mikhail 52 </li> 
+    <li>Mikhail 52 </li> 
+    <li>Mikhail 52 </li> 
+     </div>
+  </div>
+</div>
+    <div class="otz"><div class="welcome">Отзывы покупателей</div>
+<p>
+	Автор:Андрей    16.12.13</p>
+<p>
+Тут будет отзыв,давно так фильм не держал в напряжении и не заставлял сопереживать главному герою..оскар дадут однозначно</p></div>    
+<div class="timer_box">
+
+<div class="name">postel</div>
+<img src="catalog/view/theme/default/image/special_bg.png">
+<div class="price-old">2000 руб</div>
+<div class="price-new">1500 руб</div>
+
+
+<div class="timer box">
+
+<div id="product_in_timer">
+
+<img src="catalog/view/theme/default/image/time_img.png">
+</a>
+</div>
+
+
+<div class="tmr">
+  <p>
+			<!-- Начало панели счетчика -->
+		<div id="countdown_dashboard-755" class="countdown_dashboard">
+		
+			<div class="dash days_dash" style="display:none;">
+				<span class="dash_title">дней</span>
+				<div class="digit">0</div>
+				<div class="digit">0</div>
+			</div>
+
+			<div class="dash hours_dash" style="text-align:center;" >
+				<span class="dash_title">часов</span>
+				<div class="digit">0</div>
+				<div class="digit">4</div>
+			</div>
+
+			<div class="dash minutes_dash">
+				<span class="dash_title">минут</span>
+				<div class="digit">0</div>
+				<div class="digit">2</div>
+			</div>
+
+			<div class="dash seconds_dash"  >
+				<span class="dash_title" >секунд</span>
+				<div class="digit">6</div>
+				<div class="digit">0</div>
+			</div>
+
+    	</div>
+<!-- Завершение панели счетчика -->
+
+</p>
+</div>
+</div>
+</div>   
+</div>
+ 
+<div id="content"><!-- Callme-->
+<div id="callme_button">
+<a class="callme" href=" "></a>
+</div>
+<div id="callme_button2">
+<a  href=" "></a>
+<div id="callme_button3">
+<a  href=" "></a>
+</div>
+
+<!-- END Callme-->
+
+
+<div class="box box2">
+  <div class="box-heading op2">Новинки</div>
+  <div class="box-content box-content2" >
+    <div class="box-product">
+            <div>
+                <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                 <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                                <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                 <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+               <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                 <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                 <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                  <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+            <div>
+                 <div class="image"><img src="catalog/view/theme/default/image//AVKnrRmEskQ-180x240.jpg"  /></div>
+                <div class="name"><a href=" ">Asabella</a></div>
+                <div class="price">
+                    9 020p                </div>
+                       
+       <!--
+        <div class="cart"><input type="button" value="В корзину" onclick="addToCart('59');" class="button" /></div>
+         -->
+         <div class="cart cart2 ">
+    <!--        
+        <input type="button" value="В корзину" onclick="addToCart('59');" class="button" />
+   -->     
+          
+           <a  value="В корзину" onclick="addToCart('59');" ><img src="catalog/view/theme/default/image/inn.png"/></a>
+      </div>
+
+      </div>
+          </div>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+<h1 style="display: none;">Magazine</h1>
+</div>
+<div id="footer">
+
+
+	<div id="footer_menu">
+   <a href="index.php" ><img src="catalog/view/theme/default/image/google.png" alt="Google+" ></a>
+   <a href="index.php" ><img src="catalog/view/theme/default/image/vk.png" alt="Vkontake.ru" ></a>
+   <a href="index.php" ><img src="catalog/view/theme/default/image/facebook.png" alt="Facebook.com" ></a>
+   <a href="index.php" ><img src="catalog/view/theme/default/image/twitter.png" alt="Twitter.com" ></a>
+   <a href="index.php" ><img src="catalog/view/theme/default/image/klas.png" alt="Odnoklassniki.ru" ></a>
+   	
+	</div>
+
+
+<div id="foot_menu">
+<ul>
+<li><a href="index.php">Главная</a></li>
+<li><a href="index.php">О Нас</li></a>
+<li><a href="index.php">Новости</li></a>
+<li><a href="index.php">Контакты</li></a>
+<li><a href="index.php">SALE</li></a>
+</ul>
+<span>ecobellopt © 2013 Все права защищены</span>
+
+</div>
+
+<div id="footcontact">
+<img src="catalog/view/theme/default/image/footcontact.png">
+</div>
+</body></html>
